991c0dc0f3d177918d77b0c42c74d9459b0c264e
alien0077 <alien0077@gmail.com>
2026-08-24 17:11:14 +0800

    fix: mark hidden aortic root as posterior

--- heart-anatomy.js
+++ heart-anatomy.js
@@ -96,8 +96,11 @@ window.heartAnatomyLab = () => `
             <circle class="flow-particle red" r="6"><animateMotion dur="1.35s" repeatCount="indefinite"><mpath href="#flowStep6"/></animateMotion></circle>
           </g>
           <g class="flow-segment" data-flow-segment="6">
-            <!-- 從左心室中央經大動脈瓣流入畫面前方可見的主動脈根部。 -->
-            <path id="flowStep7" class="flow-line red" d="M400,500 C382,472 365,446 350,420 C330,385 285,330 246,250" marker-end="url(#flowArrowRed)"/>
+            <!-- 左心室中央先經大動脈瓣；瓣膜後的主動脈根部藏在肺動脈後方。 -->
+            <path id="flowStep7" class="flow-motion" d="M400,500 C382,472 365,446 350,420 C330,385 285,330 246,250"/>
+            <path class="flow-line red" d="M400,500 C382,472 365,446 350,420"/>
+            <path class="flow-line red flow-line-behind" d="M350,420 C330,385 285,330 246,250" marker-end="url(#flowArrowRed)"/>
+            <text class="flow-back-label" x="275" y="337">背面</text>
             <circle class="flow-particle red" r="6"><animateMotion dur="1.35s" repeatCount="indefinite"><mpath href="#flowStep7"/></animateMotion></circle>
           </g>
           <g class="flow-segment" data-flow-segment="7">
